2d30335f183210953d9af4d8e32db8bde376ea0d
Daniel Neset <danielneset@gmail.com>
2024-02-22 04:02:18 +0100

    Added swagger springdoc dependency and a test operation

--- src/main/java/no/ntnu/idata2306/y2024/g2/backend/ApiController.java
+++ src/main/java/no/ntnu/idata2306/y2024/g2/backend/ApiController.java
@@ -1,5 +1,6 @@
 package no.ntnu.idata2306.y2024.g2.backend;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("flights")
 public class ApiController {
+
   @GetMapping()
+  @Operation(summary = "Returns an string of Hei, used for initial testing.")
   public String test(){
     return "Hei";
   }
