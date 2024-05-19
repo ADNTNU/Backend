80bca17ba4600a8f14157d388fc58e7c7d2d9c0d
Anders Lund <a_lund_01@hotmail.com>
2024-05-19 20:31:31 +0200

    Fixes CORS

---
+++ src/main/java/no/ntnu/idata2306/y2024/g2/backend/config/WebConfig.java
@@ -0,0 +1,24 @@
+package no.ntnu.idata2306.y2024.g2.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Value("#{'${cors.allowedOrigins}'.split(',')}")
+  private List<String> allowedOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    String[] originsArray = allowedOrigins.toArray(new String[0]);
+    registry.addMapping("/**")
+        .allowedOrigins(originsArray)
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
+}
